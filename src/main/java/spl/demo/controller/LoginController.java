968d6407e78bb894d6645dcb3cc1a42bf2ccfe5d
taejung77 <132897696+taejung77@users.noreply.github.com>
2025-05-18 21:46:56 +0900

    로그인 한줄 수정

--- src/main/java/spl/demo/controller/LoginController.java
+++ src/main/java/spl/demo/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import spl.demo.dto.LoginDto;
 import spl.demo.dto.TokenResponse;
 import spl.demo.security.JwtUtil;
-
+@CrossOrigin(origins = {"http://localhost:5500", "http://127.0.0.1:5500"})
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
