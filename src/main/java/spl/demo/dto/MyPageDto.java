d01e3195a432278abcf98c282f8531096ac143f6
shinhyew <she8729me@naver.com>
2025-06-06 04:06:44 +0900

    마이페이지

---
+++ src/main/java/spl/demo/dto/MyPageDto.java
@@ -0,0 +1,20 @@
+package spl.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import spl.demo.entity.InterestCategoryEntity;
+import spl.demo.entity.SummaryStyle;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MyPageDto {
+    private String userId;
+    private int scrapCount;
+    private SummaryStyle style;
+    private List<InterestCategoryEntity> interestCategories;
+    private List<ScrapSummaryDto> scraps;
+}
